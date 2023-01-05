3bcab0d497702b45dd909f906d081096adf16072
Pere Jover <pjover@gmail.com>
2023-01-05 05:51:19 +0100

    Rotor position

---
+++ internal/domain/model/rotor_position/rotor_position.go
@@ -0,0 +1,24 @@
+package rotor_position
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type RotorPosition uint
+
+func (r RotorPosition) String() string {
+	return strconv.Itoa(int(r))
+}
+
+func NewRotorPosition(value uint) (RotorPosition, error) {
+
+	if value == 0 || value > 26 {
+		return RotorPosition(0), fmt.Errorf("'%d' is an invalid enigma rotor position value", value)
+	}
+	return RotorPosition(value), nil
+}
+
+func (r RotorPosition) Format() string {
+	return fmt.Sprintf("rotor position %d", r)
+}
---
+++ internal/domain/model/rotor_position/rotor_position_test.go
@@ -0,0 +1,49 @@
+package rotor_position
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewRotorPosition(t *testing.T) {
+	tests := []struct {
+		name   string
+		value  uint
+		actual RotorPosition
+		err    error
+	}{
+		{
+			name:   "Zero",
+			value:  0,
+			actual: 0,
+			err:    errors.New("'0' is an invalid enigma rotor position value"),
+		},
+		{
+			name:   "One",
+			value:  1,
+			actual: 1,
+			err:    nil,
+		},
+		{
+			name:   "Twenty six",
+			value:  26,
+			actual: 26,
+			err:    nil,
+		},
+		{
+			name:   "Twenty seven",
+			value:  27,
+			actual: 0,
+			err:    errors.New("'27' is an invalid enigma rotor position value"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewRotorPosition(tt.value)
+			assert.Equal(t, tt.actual, got)
+			assert.Equal(t, tt.err, err)
+		})
+	}
+}
