77face554eb8b1558ca02ebb3f04ea525412c524
Pere Jover <pjover@gmail.com>
2023-03-02 22:50:23 +0100

    Rotor

---
+++ internal/domain/model/rotor.go
@@ -0,0 +1,48 @@
+package model
+
+type Rotor struct {
+	number        RotorType
+	ringSetting   RingSetting
+	rotorPosition uint
+}
+
+func NewRotor(number RotorType, startingPosition StartingPosition, ringSetting RingSetting) Rotor {
+	return Rotor{
+		number:        number,
+		ringSetting:   ringSetting,
+		rotorPosition: uint(startingPosition),
+	}
+}
+
+func (r *Rotor) Number() RotorType {
+	return r.number
+}
+
+func (r *Rotor) RingSetting() RingSetting {
+	return r.ringSetting
+}
+
+func (r *Rotor) RotorPosition() uint {
+	return r.rotorPosition
+}
+
+func (r *Rotor) encipher(i uint, rotorPosition uint, ring RingSetting, wiring []uint) uint {
+	shift := rotorPosition - uint(ring)
+	return (wiring[(i+shift+26)%26] - shift + 26) % 26
+}
+
+func (r *Rotor) Forward(i uint) uint {
+	return r.encipher(i, r.rotorPosition, r.ringSetting, r.number.ForwardWiring())
+}
+
+func (r *Rotor) Backward(i uint) uint {
+	return r.encipher(i, r.rotorPosition, r.ringSetting, r.number.InverseWiring())
+}
+
+func (r *Rotor) IsAtNotch() bool {
+	return r.number.NotchPosition() == r.rotorPosition
+}
+
+func (r *Rotor) TurnOver() {
+	r.rotorPosition = (r.rotorPosition + 1) % 26
+}
---
+++ internal/domain/model/rotor_test.go
@@ -0,0 +1,127 @@
+package model
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRotor_TurnOver(t *testing.T) {
+	type fields struct {
+		number        RotorType
+		ringSetting   RingSetting
+		rotorPosition uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   uint
+	}{
+		{
+			name: "Case 0",
+			fields: fields{
+				I,
+				RingSetting(uint(1)),
+				0,
+			},
+			want: uint(1),
+		},
+		{
+			name: "Case 1",
+			fields: fields{
+				I,
+				RingSetting(uint(1)),
+				1,
+			},
+			want: uint(2),
+		},
+		{
+			name: "Case 24",
+			fields: fields{
+				I,
+				RingSetting(uint(2)),
+				24,
+			},
+			want: uint(25),
+		},
+		{
+			name: "Case 25",
+			fields: fields{
+				I,
+				RingSetting(uint(3)),
+				25,
+			},
+			want: uint(0),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Rotor{
+				number:        tt.fields.number,
+				ringSetting:   tt.fields.ringSetting,
+				rotorPosition: tt.fields.rotorPosition,
+			}
+			r.TurnOver()
+			assert.Equal(t, tt.want, r.rotorPosition)
+		})
+	}
+}
+
+func TestRotor_IsAtNotch(t *testing.T) {
+	type fields struct {
+		number        RotorType
+		ringSetting   RingSetting
+		rotorPosition uint
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "Is a notch",
+			fields: fields{
+				I,
+				RingSetting(uint(2)),
+				16,
+			},
+			want: true,
+		},
+		{
+			name: "Is not a notch",
+			fields: fields{
+				I,
+				RingSetting(uint(2)),
+				17,
+			},
+			want: false,
+		},
+		{
+			name: "Is a notch",
+			fields: fields{
+				VI,
+				RingSetting(uint(2)),
+				0,
+			},
+			want: true,
+		},
+		{
+			name: "Is not a notch",
+			fields: fields{
+				VI,
+				RingSetting(uint(2)),
+				17,
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Rotor{
+				number:        tt.fields.number,
+				ringSetting:   tt.fields.ringSetting,
+				rotorPosition: tt.fields.rotorPosition,
+			}
+			assert.Equalf(t, tt.want, r.IsAtNotch(), "IsAtNotch()")
+		})
+	}
+}
