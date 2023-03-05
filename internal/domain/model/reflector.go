248df441a2810fc62d58a04af9b4f04a800f43ea
Pere Jover <pjover@gmail.com>
2023-03-05 20:36:31 +0100

    Reflector

--- internal/domain/model/reflector.go
+++ internal/domain/model/reflector.go
@@ -41,16 +41,10 @@ var reflectorForwardWiringValues = [][]int{
 	{5, 21, 15, 9, 8, 0, 14, 24, 4, 3, 17, 25, 23, 22, 6, 2, 19, 10, 20, 16, 18, 1, 13, 12, 7, 11},
 }
 
-func (r Reflector) ForwardWiring() []int {
+func (r Reflector) forwardWiring() []int {
 	return reflectorForwardWiringValues[r]
 }
 
-var reflectorInverseWiringValues = [][]int{
-	{25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
-	{24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19},
-	{5, 21, 15, 9, 8, 0, 14, 24, 4, 3, 17, 25, 23, 22, 6, 2, 19, 10, 20, 16, 18, 1, 13, 12, 7, 11},
-}
-
-func (r Reflector) InverseWiring() []int {
-	return reflectorInverseWiringValues[r]
+func (r Reflector) Forward(value int) int {
+	return reflectorForwardWiringValues[r][value]
 }
--- internal/domain/model/reflector_test.go
+++ internal/domain/model/reflector_test.go
@@ -86,52 +86,60 @@ func TestReflector_ForwardWiring(t *testing.T) {
 			name:      "A",
 			reflector: A,
 			//want: decodeWiring(A.encoding()),
-			want: A.ForwardWiring(),
+			want: A.forwardWiring(),
 		},
 		{
 			name:      "B",
 			reflector: B,
 			//want: decodeWiring(B.encoding()),
-			want: B.ForwardWiring(),
+			want: B.forwardWiring(),
 		},
 		{
 			name:      "C",
 			reflector: C,
 			//want: decodeWiring(C.encoding()),
-			want: C.ForwardWiring(),
+			want: C.forwardWiring(),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, tt.reflector.ForwardWiring(), "ForwardWiring()")
+			assert.Equalf(t, tt.want, tt.reflector.forwardWiring(), "forwardWiring()")
 		})
 	}
 }
 
-func TestReflector_InverseWiring(t *testing.T) {
+func TestReflector_Forward(t *testing.T) {
 	tests := []struct {
-		name   string
-		wiring []int
-		want   []int
+		name  string
+		r     Reflector
+		value int
+		want  int
 	}{
-		{
-			"A",
-			A.ForwardWiring(),
-			A.InverseWiring(),
-		},
-		{
-			"B",
-			B.ForwardWiring(),
-			B.InverseWiring()},
-		{
-			"C",
-			C.ForwardWiring(),
-			C.InverseWiring()},
+		{"A: 0", A, 0, 25},
+		{"A: 1", A, 1, 24},
+		{"A: 7", A, 7, 18},
+		{"A: 13", A, 13, 12},
+		{"A: 20", A, 20, 5},
+		{"A: 24", A, 24, 1},
+		{"A: 25", A, 25, 0},
+		{"B: 0", B, 0, 24},
+		{"B: 1", B, 1, 17},
+		{"B: 7", B, 7, 3},
+		{"B: 13", B, 13, 10},
+		{"B: 20", B, 20, 2},
+		{"B: 24", B, 24, 0},
+		{"B: 25", B, 25, 19},
+		{"C: 0", C, 0, 5},
+		{"C: 1", C, 1, 21},
+		{"C: 7", C, 7, 24},
+		{"C: 13", C, 13, 22},
+		{"C: 20", C, 20, 18},
+		{"C: 24", C, 24, 7},
+		{"C: 25", C, 25, 11},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := inverseWiring(tt.wiring)
-			assert.Equal(t, tt.want, got)
+			assert.Equalf(t, tt.want, tt.r.Forward(tt.value), "Forward(%v)", tt.value)
 		})
 	}
 }
